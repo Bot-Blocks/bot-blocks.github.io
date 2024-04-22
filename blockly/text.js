22c1a2608ac8703337321da2b6fd2032f7fbd873
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2024-04-22 18:06:08 -0300

    fix

---
+++ blockly/text.js
@@ -0,0 +1,15 @@
+Blockly.Blocks['text_newline'] = {
+    init: function () {
+        this.appendDummyInput()
+            .appendField("newline");
+        this.setOutput(true, "String");
+        this.setColour(165);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+javascript.javascriptGenerator.forBlock['text_newline'] = function (block, generator) {
+    var code = '\n';
+    return [code, javascript.Order.NONE];
+};
