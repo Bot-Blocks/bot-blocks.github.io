bb46c0614e099551da11c920d811f493040acf13
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2024-04-22 00:48:47 -0300

    Update main.js

--- blockly/main.js
+++ blockly/main.js
@@ -11,6 +11,29 @@ Blockly.Blocks['main_bottoken'] = {
 
 javascript.javascriptGenerator.forBlock['main_bottoken'] = function(block, generator) {
   var value_token = generator.valueToCode(block, 'TOKEN', javascript.Order.ATOMIC);
+  
   var code = `client.login(${value_token})`;
+  
   return code;
+};
+
+Blockly.Blocks['process_env'] = {
+  init: function() {
+    this.appendValueInput("ENV")
+        .setCheck("String")
+        .appendField("process env");
+    this.setInputsInline(true);
+    this.setOutput(true, "Env");
+    this.setColour(270);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+javascript.javascriptGenerator.forBlock['process_env'] = function(block, generator) {
+  var value_env = generator.valueToCode(block, 'ENV', javascript.Order.ATOMIC);
+  
+  var code = `process.env[${value_env}]`;
+  
+  return [code, javascript.Order_NONE];
 };
