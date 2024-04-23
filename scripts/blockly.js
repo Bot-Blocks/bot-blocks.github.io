f5a0234116ce29909b59d8ba3430cb8ce855b2fd
ddededodediamante <111385835+ddededodediamante@users.noreply.github.com>
2024-04-23 17:36:26 -0300

    fixxx

--- scripts/blockly.js
+++ scripts/blockly.js
@@ -59,7 +59,7 @@ function saveRecoverXmlProject() {
 }
 
 function updateCode(event) {
-  if (event.type == null) event.type == 'event';
+  if (event == null) event == { type:'event' };
   if (event.type == 'viewport_change' || event.type == 'toolbox_item_select') return;
 
   localSaveCount -= 1;
